71a42c679166c360894e37cce24bdf41e7f2061b
victorpoliushko <victor.poliushko@gmail.com>
2026-03-21 12:27:40 +0200

    ssl fix

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,15 @@
-import { Pool } from 'pg';
-import * as dotenv from 'dotenv';
+import { Pool } from "pg";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
+const isLocal =
+  process.env.DB_URL?.includes("localhost") ||
+  process.env.DB_URL?.includes("127.0.0.1");
+
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: process.env.DB_HOST === 'localhost' || process.env.DB_HOST === '127.0.0.1' 
-       ? false 
-       : { rejectUnauthorized: false }
+  ssl: isLocal ? false : { rejectUnauthorized: false },
 });
 
 export default pool;
